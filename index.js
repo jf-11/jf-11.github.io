2e688da4587f84020b93b11bfb6486ba78b65e59
Joël Fehr <joel.fehr@bluewin.ch>
2024-01-10 15:31:31 +0100

    font issue ios

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
-    var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
-    var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
-    var is_safari = navigator.userAgent.indexOf("Safari") > -1;
-    var is_opera = navigator.userAgent.indexOf("Presto") > -1;
-    var is_mac = navigator.userAgent.indexOf('Mac OS') != -1;
-    var is_windows = !is_mac;
-
-    if (is_chrome && is_safari){
-        is_safari = false;
-    }
-
-    if (is_safari || is_windows){
-        document.body.style.webkitTextStroke = '0.5px';
-    }
-});
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const checkbox = document.getElementById("checkbox");
     const body = document.body;
